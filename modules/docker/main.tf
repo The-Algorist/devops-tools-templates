f20393cd7a05f458d9aa2bc444a2bb0ac69a8150
The-Algorist <waruithemystery@gmail.com>
2024-02-03 12:38:58 +0300

    Add egress and ingress resources as well as security group

--- modules/docker/main.tf
+++ modules/docker/main.tf
@@ -16,11 +16,47 @@ provider "aws" {
 
 resource "aws_instance" "docker-instance" {
     ami           = "ami-015e832ac6a60f0de"
-    instance_type = "t2.medium"
+    instance_type = "t2.micro"
     user_data     = file("docker-install.sh")
   
     tags = {
         Name = "docker"
     }
   
+}
+
+resource "aws_security_group" "docker" {
+    name = "docker"
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
 }
