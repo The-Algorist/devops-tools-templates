df5c321f4f4e6328fe26de62ba879687deb3bbdf
The-Algorist <waruithemystery@gmail.com>
2024-02-03 13:27:12 +0300

    Add docker and docker-compose installation files

--- modules/docker/docker-install.sh
+++ modules/docker/docker-install.sh
@@ -4,3 +4,12 @@ sudo yum update -y
 sudo yum install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+
+# install docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+
